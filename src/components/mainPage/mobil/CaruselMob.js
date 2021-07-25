9235da64cac4c8dd6a6cde9368481989450d58ae
panyasnap <panyasnap@mail.ru>
2021-07-26 01:27:04 +0300

    webc

--- src/components/mainPage/mobil/CaruselMob.js
+++ src/components/mainPage/mobil/CaruselMob.js
@@ -14,13 +14,13 @@ const CaruselMob = () => {
             <div className='container position-relative '>
                 <div className='w-100 mb-2'>
                     <h3 className={`${style.h3textMob}`}>
-                        <div><span className={`${style.spanMob}`}>Дизайн, изготовление мебели</span></div>
-                        <span className={`${style.spanMob} pb-1`}> и интерьера на заказ
+                        <div><span className={`${style.h3spanMob}`}>Дизайн, изготовление мебели</span></div>
+                        <span className={`${style.h3spanMob} pb-1`}> и интерьера на заказ
                                 </span></h3>
                 </div>
                 <div className='w-70 position-relative mb-1'>
                     <div className={`${style.ptextMob} `}>
-                        <div><span className={`${style.pspanMob} pb-0`}>Создаем уникальные интерьеры и мебель премиум</span>
+                        <div><span className={`${style.spanMob} `}>Создаем уникальные интерьеры и мебель премиум</span>
                         </div>
                         <span className={`${style.spanMob} pb-1`}> класса в Москве и
                                 Санкт-Петербурге</span></div>
